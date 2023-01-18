78c1e87f2625baad073e93c9d8177d72c1a240bb
Zeynep <zeynepkargi02@gmail.com>
2023-01-18 13:55:56 +0300

    ders11

---
+++ Ders_5/2.py
@@ -0,0 +1,31 @@
+import os
+import zipfile
+import pandas as pd
+import sqlite3
+import matplotlib.pyplot as plt
+
+bag = sqlite3.connect("kripto.vt")
+cursor = bag.cursor()
+
+
+parite = "AVAXUSDT"  # input("Parite Giriniz :")
+bas_tarih = "2022-02-03"  #  input("Başlangıç Tarihi :")
+bit_tarih = "2022-02-04"  # input("Bitiş Tarihi :")
+
+sorgu = "SELECT * FROM parite WHERE " \
+        "(otime BETWEEN '"+bas_tarih+"' " \
+        "AND '"+bit_tarih+"') " \
+        "AND parite='"+parite+"'"
+cursor.execute(sorgu)
+sonuc = cursor.fetchall()
+liste_x = []
+liste_y = []
+for mum in sonuc:
+    #print(mum)
+    #print(mum[1])
+    liste_y.append(mum[6])
+    liste_x.append(mum[2])
+
+plt.plot(liste_x, liste_y)
+plt.show()
+bag.close()
